7132d20d48b678e6202990b4e18627ed96c8335c
zilva149 <114057257+zilva149@users.noreply.github.com>
2022-11-13 13:17:22 +0200

    Toggle functionality

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,10 +1,19 @@
-// IMPORTS
+// ******** IMPORTS **********
 
-// EXECUTION
+// ********** EXECUTION *********
 
+// Navbar
 const navToggle = document.getElementById('nav-toggle');
-const navContainer = document.getElementById('nav');
+const nav = document.getElementById('nav');
+const navContainer = document.getElementById('nav-container');
 
 navToggle.addEventListener('click', () => {
-    navContainer.classList.toggle('show-nav');
+    const navHeight = nav.getBoundingClientRect().height;
+    if (!navContainer.classList.contains('show-nav')) {
+        navContainer.classList.add('show-nav');
+        navContainer.style.height = `${navHeight + 20}px`;
+    } else {
+        navContainer.classList.remove('show-nav');
+        navContainer.style.height = `0`;
+    }
 });
