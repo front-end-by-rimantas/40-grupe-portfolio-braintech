41bed1a3f0a528116d6808c41abbfc06b70bf8d1
zilva149 <114057257+zilva149@users.noreply.github.com>
2022-11-13 12:50:07 +0200

    Final structural and style changes

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,3 +1,10 @@
 // IMPORTS
 
 // EXECUTION
+
+const navToggle = document.getElementById('nav-toggle');
+const navContainer = document.getElementById('nav');
+
+navToggle.addEventListener('click', () => {
+    navContainer.classList.toggle('show-nav');
+});
