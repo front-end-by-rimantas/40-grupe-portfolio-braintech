2df5c7eab4c6632018815fbd082c07e0337bddd2
zilva149 <114057257+zilva149@users.noreply.github.com>
2022-11-13 13:43:45 +0200

    Resize behavior done

--- js/pages/home.js
+++ js/pages/home.js
@@ -2,16 +2,25 @@
 
 // ********** EXECUTION *********
 
-// Navbar
+// navbar variables
 const navToggle = document.getElementById('nav-toggle');
 const nav = document.getElementById('nav');
 const navContainer = document.getElementById('nav-container');
+
+window.addEventListener('resize', () => {
+    if (navContainer.classList.contains('show-nav')) {
+        navContainer.classList.remove('show-nav');
+        navContainer.style.height = `0`;
+        navContainer.style.opacity = `0`;
+    }
+});
 
 navToggle.addEventListener('click', () => {
     const navHeight = nav.getBoundingClientRect().height;
     if (!navContainer.classList.contains('show-nav')) {
         navContainer.classList.add('show-nav');
         navContainer.style.height = `${navHeight + 20}px`;
+        navContainer.style.opacity = `1`;
     } else {
         navContainer.classList.remove('show-nav');
         navContainer.style.height = `0`;
